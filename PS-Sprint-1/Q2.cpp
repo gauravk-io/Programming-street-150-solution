23d80ba892bb9f9a084cee6ac549f74b15f57eed
Gaurav <gauravkr7265@gmail.com>
2024-12-16 07:20:30 +0530

    q2 solved

--- PS-Sprint-1/Q2.cpp
+++ PS-Sprint-1/Q2.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 int main() {
     // Check for Prime number
-    
+      int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    if(num == 0 || num == 1) {
+        std::cout << "Neither Prime nor Composite";
+    } else {
+        bool isPrime = true;
+        for(int i = 2; i <= num / 2; i++) {
+            if(num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) {
+            std::cout << "PRIME" << std::endl;
+        } else {
+            std::cout << "COMPOSITE";
+        }
+    }
+  
+
 
     return 0;
 }
