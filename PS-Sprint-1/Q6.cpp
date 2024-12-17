1247c9d5c550283da0fad2ac71fb888c033ecc5f
Gaurav <gauravkr7265@gmail.com>
2024-12-17 18:48:22 +0530

    Q6 solved and added

---
+++ PS-Sprint-1/Q6.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//  Identifyinf Palindrome
+
+bool isPalindrome(const string& str){
+    int start = 0;
+    int end = str.length() - 1;
+    while(start < end){
+        if (str[start] != str[end]){
+        return false;
+        }
+        start++;
+        end--;
+
+    }
+    return true;
+}
+
+
+
+int main(){
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (isPalindrome(str)) {
+        cout << str << " is a palindrome." << endl;
+    } else {
+        cout << str << " is not a palindrome." << endl;
+    }
+
+    return 0;
+
+}
