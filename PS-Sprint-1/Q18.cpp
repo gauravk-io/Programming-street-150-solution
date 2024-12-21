57df5e917edd645dbf0ba07c17d9d587b049189f
Gaurav <gauravkr7265@gmail.com>
2024-12-21 14:38:45 +0530

    Q18 solved and added

---
+++ PS-Sprint-1/Q18.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cout << "ENTER THE NUMBER : ";
+cin >> n;
+for (int i=1; i<=10; i++){
+    cout << n << " x " << i << " = " << n*i<< endl;
+}
+
+
+}
+
